6591437690cffd286acc7796ceee30306564bfc3
Choi Hong Gyu <zxolqs@gmail.com>
2023-02-02 21:54:56 +0900

    [#9] feat : 글로벌 스타일 및 테마 추가
    - 기존 Vite 프로젝트 생성 시 초기 설치되어있던 파일 제거 (App.css, App.tsx, ...)
    - GlobalStyle 컴포넌트 생성
    - theme 파일 추가
    - GlobalStyle 컴포넌트 storybook 연동
    
    Co-authored-by: unsnruu <unsnruu@gmail.com>

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,14 @@
+import React from "react";
+import GlobalStyle from "../src/styles/GlobalStyle";
+
+export const decorators = [
+  Story => {
+    return React.createElement(React.Fragment, {
+      children: [React.createElement(GlobalStyle), React.createElement(Story)],
+    });
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +17,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
