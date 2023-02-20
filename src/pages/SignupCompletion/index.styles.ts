024a894bcf0c77c8280ef49c2140687f5d23f884
unsnruu <unsnruu@gmail.com>
2023-02-20 21:53:57 +0900

    [#26] feat : SignupCompletion 페이지 스타일링 구현
    SignupCompletion를 구현하기 위해 구체적인 스타일을 구현했습니다.

--- src/pages/SignupCompletion/index.styles.ts
+++ src/pages/SignupCompletion/index.styles.ts
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+import { color, font } from "@/styles/theme";
 
 export const Container = styled.div`
   display: flex;
+  height: 100%;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   & > img {
     width: 75px;
     height: 75px;
@@ -14,9 +17,40 @@ export const Welcome = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  line-height: 23px;
+  margin-top: 21px;
+  margin-bottom: 67px;
 `;
-export const Guides = styled.div``;
-export const Guide = styled.div``;
-export const Alignment = styled.div`
+export const Guides = styled.div`
+  background-color: ${color.light_gray3};
+  padding: 5px 20px;
+  margin-bottom: 50px;
+`;
+
+export const Guide = styled.div`
   display: flex;
+  padding: 15px 0;
+  align-items: center;
+`;
+
+export const ImgWrapper = styled.div`
+  width: 62px;
+  height: 65px;
+  background-color: white;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 18px;
+`;
+
+export const GuideText = styled.div`
+  flex: 1;
+  font-size: ${font.size.f4};
+  word-break: keep-all;
+  line-height: 15px;
+  & > span {
+    font-weight: bold;
+    color: ${color.purple};
+  }
 `;
--- src/pages/SignupCompletion/index.tsx
+++ src/pages/SignupCompletion/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 import Text from "@/@components/Atoms/Text";
+import Divider from "@/@components/Atoms/Divider";
+import FlexBox from "@/@components/Layout/FlexBox";
 import * as Styled from "./index.styles";
+import Button from "@/@components/Atoms/Button";
 
 const SignupCompletion = () => {
   const email = "username@email.com";
@@ -11,47 +14,44 @@ const SignupCompletion = () => {
       <img src="./img-thumb-up.png" alt="thumb-up" />
       <Styled.Welcome>
         <Text variant="f2-regular">반갑습니다</Text>
-        <Styled.Alignment>
+        <FlexBox direction="row">
           <Text variant="f2-bold" color="purple">
             {email}
           </Text>
           <Text variant="f2-regular">님</Text>
-        </Styled.Alignment>
+        </FlexBox>
         <Text variant="f2-regular">JJAN 회원이 되신 걸 축하드립니다!</Text>
       </Styled.Welcome>
       <Styled.Guides>
         <Styled.Guide>
-          <img src="" alt="" />
-          <Styled.Alignment>
-            <Text variant="f4-bold" color="purple">
-              커뮤니티 서비스
-            </Text>
-            <Text variant="f4-regular">
-              를 통해 술에 대한 정보를 공유해보세요!
-            </Text>
-          </Styled.Alignment>
+          <Styled.ImgWrapper>
+            <img src="./img-conversation.png" alt="" />
+          </Styled.ImgWrapper>
+          <Styled.GuideText>
+            <span>커뮤니티 서비스</span>를 통해 술에 대한 정보를 공유해보세요!
+          </Styled.GuideText>
         </Styled.Guide>
+        <Divider color="dark_gray" />
         <Styled.Guide>
-          <img src="" alt="" />
-          <Styled.Alignment>
-            <Text variant="f4-regular">현 위치를 기반으로</Text>
-            <Text variant="f4-bold" color="purple">
-              내 주변 술메이트
-            </Text>
-            <Text variant="f4-regular">를 찾아보세요!</Text>
-          </Styled.Alignment>
+          <Styled.ImgWrapper>
+            <img src="./img-cheers.png" alt="" />
+          </Styled.ImgWrapper>
+          <Styled.GuideText>
+            현 위치를 기반으로 <span>내 주변 술메이트</span>를 찾아보세요!
+          </Styled.GuideText>
         </Styled.Guide>
+        <Divider color="dark_gray" />
         <Styled.Guide>
-          <img src="" alt="" />
-          <Styled.Alignment>
-            <Text variant="f4-regular">지역기반 커뮤니티를 통해</Text>
-            <Text variant="f4-bold" color="purple">
-              내 지역 핫한 술집
-            </Text>
-            <Text variant="f4-regular">을 공유해보세요!</Text>
-          </Styled.Alignment>
+          <Styled.ImgWrapper>
+            <img src="./img-location.png" alt="" />
+          </Styled.ImgWrapper>
+          <Styled.GuideText>
+            지역기반 커뮤니티를 통해 <span>내 지역 핫한 술집</span>을
+            공유해보세요!
+          </Styled.GuideText>
         </Styled.Guide>
       </Styled.Guides>
+      <Button height="l" shape="purple" text="로그인 하기"></Button>
     </Styled.Container>
   );
 };
