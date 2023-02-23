f48325efde477fbb07ecaf4d923cb26e61b349af
unsnruu <unsnruu@gmail.com>
2023-02-23 21:18:16 +0900

    [#26] Header 컴포넌트의 스타일 수정
    Header의 background-color 값을 추가하였습니다.

--- src/@components/Molecule/Header/index.styles.ts
+++ src/@components/Molecule/Header/index.styles.ts
@@ -7,6 +7,7 @@ export const Container = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: white;
 `;
 
 export const IconWrapper = styled.div`
