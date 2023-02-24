80b02673af50f4372f6347dae8ba1e53f31b5cf9
unsnruu <unsnruu@gmail.com>
2023-02-24 21:16:05 +0900

    [#43] FlexBox 컴포넌트 구현
    트리플 프론트엔드 깃허브를 참고하여 FlexBox컴포넌트를 구현했습니다.
    이후 추가되는 요구사항이 있을 시에 컴포넌트를 확장하도록 하겠습니다.

---
+++ src/@components/Atoms/Flexbox/index.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { HTMLAttributes } from "react";
+import { Property } from "csstype";
+import { Base, BaseProps } from "../Base";
+
+interface FlexBoxProps extends BaseProps, HTMLAttributes<Element> {
+  flexDirection?: Property.FlexDirection;
+  flexWrap?: Property.FlexWrap;
+  justifyContent?: Property.JustifyContent;
+  alignItems?: Property.AlignItems;
+  gap?: Property.Gap;
+  columnGap?: Property.ColumnGap;
+  rowGap?: Property.RowGap;
+}
+
+export const FlexBox = styled(Base)<FlexBoxProps>(props => ({
+  display: "flex",
+  flexDirection: props.flexDirection,
+  justifyContent: props.justifyContent,
+  alignItems: props.alignItems,
+  flexWrap: props.flexWrap,
+  gap: props.gap,
+  columnGap: props.columnGap,
+  rowGap: props.rowGap,
+}));
