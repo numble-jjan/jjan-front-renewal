a2a5d539d2430c7ea42e0ce69eaaa52a774a31e7
unsnruu <unsnruu@gmail.com>
2023-02-25 12:07:37 +0900

    [#43] Base의 story 구현

---
+++ src/@components/Atoms/Base/index.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Base from ".";
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+  title: "Core/Base",
+  component: Base,
+} as ComponentMeta<typeof Base>;
+
+export const Default: ComponentStory<typeof Base> = args => {
+  return <Base {...args}>TEXT</Base>;
+};
+Default.args = {
+  backgroundColor: "white",
+  borderRadius: 10,
+  border: "1px solid black",
+  padding: "10px",
+};
