40943f4310f0ab173238023a4a359de41d21abbb
unsnruu <unsnruu@gmail.com>
2023-02-25 11:42:41 +0900

    [#43] FlexBox의 props 네이밍 수정

--- src/@components/Atoms/Flexbox/index.tsx
+++ src/@components/Atoms/Flexbox/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as Styled from "./index.styles";
 
-export const FlexBox = ({ ...rest }: Styled.FlexBoxProps) => {
-  return <Styled.FlexBox {...rest}></Styled.FlexBox>;
+export const FlexBox = (props: Styled.FlexBoxProps) => {
+  return <Styled.FlexBox {...props}></Styled.FlexBox>;
 };
