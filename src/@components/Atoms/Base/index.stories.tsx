8ef9baf3a9687266596226e47e15c2b9f9b21129
unsnruu <unsnruu@gmail.com>
2023-02-25 13:24:33 +0900

    [#43] Base 스토리북에 argType 정의

--- src/@components/Atoms/Base/index.stories.tsx
+++ src/@components/Atoms/Base/index.stories.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Base from "./index";
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
+import { colorMap } from "@/styles/theme";
 
 export default {
   title: "Core/Base",
   component: Base,
+  argTypes: {
+    backgroundColor: { control: "select", options: Object.keys(colorMap) },
+  },
 } as ComponentMeta<typeof Base>;
 
 export const Default: ComponentStory<typeof Base> = args => {
