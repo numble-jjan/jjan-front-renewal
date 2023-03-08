14585d9bfcd423b6e0ce583a3d99fb4f927cfdcb
unsnruu <unsnruu@gmail.com>
2023-03-08 21:25:18 +0900

    [#55] feat: 여러 이미지에 대응한 Story를 보여 줄 수 있도록 수정

--- src/@components/Core/Avartar/index.stories.tsx
+++ src/@components/Core/Avartar/index.stories.tsx
@@ -1,23 +1,51 @@
 import React from "react";
 import Avartar from "./index";
-import type { ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+const IMG_SAMPLES = {
+  sample1: "./img-character.png",
+  sample2: "./img-numble-logo.jpg",
+  sample3: "./img-avartar-sample-1.jpeg",
+  sample4: "./img-avartar-sample-2.jpeg",
+  sample5: "./img-avartar-sample-3.jpg",
+};
 
 export default {
   title: "Core/Avartar",
   component: Avartar,
+  argTypes: {
+    src: {
+      control: "select",
+      options: Object.keys(IMG_SAMPLES),
+      mapping: IMG_SAMPLES,
+    },
+  },
 } as ComponentMeta<typeof Avartar>;
 
-const IMG_SAMPLE = "./img-character.png";
+export const Comment: ComponentStory<typeof Avartar> = ({ ...args }) => {
+  return <Avartar {...args} variant="comment"></Avartar>;
+};
+Comment.args = {
+  src: IMG_SAMPLES.sample1,
+};
 
-export const Author = () => {
-  return <Avartar src={IMG_SAMPLE} variant="author" />;
+export const Author: ComponentStory<typeof Avartar> = ({ ...args }) => {
+  return <Avartar {...args} variant="author"></Avartar>;
 };
-export const Comment = () => {
-  return <Avartar src={IMG_SAMPLE} variant="comment" />;
+Author.args = {
+  src: IMG_SAMPLES.sample1,
 };
-export const Profile = () => {
-  return <Avartar src={IMG_SAMPLE} variant="profile" />;
+
+export const Profile: ComponentStory<typeof Avartar> = ({ ...args }) => {
+  return <Avartar {...args} variant="profile"></Avartar>;
 };
-export const Setting = () => {
-  return <Avartar src={IMG_SAMPLE} variant="setting" />;
+Profile.args = {
+  src: IMG_SAMPLES.sample1,
+};
+
+export const Setting: ComponentStory<typeof Avartar> = ({ ...args }) => {
+  return <Avartar {...args} variant="setting"></Avartar>;
+};
+Setting.args = {
+  src: IMG_SAMPLES.sample1,
 };
