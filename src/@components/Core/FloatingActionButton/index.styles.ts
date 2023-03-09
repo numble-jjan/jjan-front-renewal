e121fbfb92d5d58d02ce8551a82771887e3fb897
unsnruu <unsnruu@gmail.com>
2023-03-09 21:32:36 +0900

    [#62] feat : 기본적인 UI와 style의 구현

---
+++ src/@components/Core/FloatingActionButton/index.styles.ts
@@ -0,0 +1,30 @@
+import styled, { css } from "styled-components";
+import { color } from "@/styles/theme";
+import type { Props, FabVariant } from "./index";
+
+type ContainerProps = Omit<Props, "onClick">;
+
+const fabVariant: Record<FabVariant, ReturnType<typeof css>> = {
+  up: css`
+    width: 40px;
+    height: 40px;
+    border: 1px solid ${color.light_gray1};
+  `,
+  write: css`
+    width: 50px;
+    height: 50px;
+    background-color: ${color.purple};
+  `,
+};
+
+export const Container = styled.div<ContainerProps>`
+  position: fixed;
+  left: ${props => props.left};
+  top: ${props => props.top};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  transform: translate(-100%, -100%);
+  ${prop => fabVariant[prop.variant]}
+`;
