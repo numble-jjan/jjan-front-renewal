6b4e4e310c04fd7d95d996638e894f160caf4c18
unsnruu <unsnruu@gmail.com>
2023-03-09 19:46:51 +0900

    [#59] feat : src의 default 값 설정과 onError 핸들러 작성

--- src/@components/Core/Avartar/index.tsx
+++ src/@components/Core/Avartar/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import * as Styled from "./index.styles";
-
 import type { AvartarFormat } from "@/types/avartar";
 
 interface AvartarProps {
@@ -8,10 +7,18 @@ interface AvartarProps {
   variant: AvartarFormat;
 }
 
-const Avartar = ({ src, variant }: AvartarProps) => {
+const DEFAULT_SRC = "./img-anonymous.png";
+
+const Avartar = ({ src = DEFAULT_SRC, variant }: AvartarProps) => {
+  const handleError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    if (e.target instanceof HTMLImageElement) {
+      e.target.src = DEFAULT_SRC;
+    }
+  };
+
   return (
     <Styled.Wrapper variant={variant}>
-      <img src={src} />
+      <img src={src} onError={handleError} />
     </Styled.Wrapper>
   );
 };
