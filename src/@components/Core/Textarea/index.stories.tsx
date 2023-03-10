3f01365250e080de61e3fa61d9d8cb519a2963d0
unsnruu <unsnruu@gmail.com>
2023-03-10 21:39:14 +0900

    [#75] feat : story에 rows값 반영

--- src/@components/Core/Textarea/index.stories.tsx
+++ src/@components/Core/Textarea/index.stories.tsx
@@ -6,14 +6,25 @@ import type { ComponentMeta, ComponentStory } from "@storybook/react";
 export default {
   title: "Core/Textarea",
   component: Textarea,
+  argTypes: {
+    rows: {
+      control: "number",
+    },
+  },
 } as ComponentMeta<typeof Textarea>;
 
 export const Default: ComponentStory<typeof Textarea> = args => (
   <Textarea {...args} />
 );
+Default.args = {
+  rows: 3,
+};
 
 export const Wrapped: ComponentStory<typeof Textarea> = args => (
   <Base height="100px" border="1px solid black">
     <Textarea {...args} />
   </Base>
 );
+Wrapped.args = {
+  rows: 3,
+};
