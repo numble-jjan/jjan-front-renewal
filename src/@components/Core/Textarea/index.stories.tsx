3ca4833a42581b55453b2bd03dd569b421d1970f
unsnruu <unsnruu@gmail.com>
2023-03-10 20:19:59 +0900

    [#67] feat : Wrapped 스토리 추가

--- src/@components/Core/Textarea/index.stories.tsx
+++ src/@components/Core/Textarea/index.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Textarea from "./index";
+import Base from "../Base";
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default { title: "Core/Textarea", component: Textarea } as ComponentMeta<
@@ -8,4 +9,10 @@ export default { title: "Core/Textarea", component: Textarea } as ComponentMeta<
 
 export const Deafult: ComponentStory<typeof Textarea> = args => (
   <Textarea {...args} />
+);
+
+export const Wrapped: ComponentStory<typeof Textarea> = args => (
+  <Base height="100px" border="1px solid black">
+    <Textarea {...args} />
+  </Base>
 );
