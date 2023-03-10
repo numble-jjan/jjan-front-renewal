1e67f02a7cd379973955ae0c76cf9baeebad3527
unsnruu <unsnruu@gmail.com>
2023-03-10 21:38:24 +0900

    [#75] feat : rows가 존재할 경우 height보다 항상 우선시 되게끔 적용

--- src/@components/Core/Textarea/index.styles.ts
+++ src/@components/Core/Textarea/index.styles.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.textarea`
+/**
+ * rows props이 존재한다면
+ * 언제나 rows의 크기가 height보다 앞서서 적용됨
+ */
+
+export const Container = styled.textarea<{ rows?: number }>`
   width: 100%;
-  height: 100%;
+  height: ${({ rows }) => (rows ? "auto" : "100%")};
   word-break: keep-all;
   resize: none;
   white-space: normal;
