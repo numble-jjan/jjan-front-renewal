12a683d7b099f683f58e25e5cfac4924b95eba00
unsnruu <unsnruu@gmail.com>
2023-03-10 21:34:51 +0900

    [#75] feat : rows prop을 선택적으로 바꿀 수 있도록 변경

--- src/@components/Core/Textarea/index.tsx
+++ src/@components/Core/Textarea/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import * as Styled from "./index.styles";
 
 interface Props {
-  rows: number;
+  rows?: number;
   onChange?: () => void;
 }
 
-const Textarea = ({ rows, ...props }: Props) => {
-  return <Styled.Container rows={rows} {...props} />;
+const Textarea = (props: Props) => {
+  return <Styled.Container {...props} />;
 };
 
 export default Textarea;
