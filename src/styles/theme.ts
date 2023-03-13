2b774cf820527113a6719a9929a8f8eedca3209d
unsnruu <unsnruu@gmail.com>
2023-03-13 21:24:24 +0900

    [#77] 스토리북 오류를 해결하기 위해 임의로 기존 코드 복원

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -86,3 +86,54 @@ export const buttonHeight = {
   m: "40px",
   l: "50px",
 };
+
+/**
+ * 추후 삭제 예정
+ */
+import type { Variant, Variants } from "@/types/textVariant";
+
+const { size, weight } = font;
+
+type FontSizeKey = keyof FontSize;
+type FontWeightKey = keyof FontWeight;
+
+const createTextVariants = (fontSizes: FontSize, fontWeights: FontWeight) => {
+  const variants = {} as Variants;
+
+  const sizeKeys = Object.keys(fontSizes) as FontSizeKey[];
+  const weightKeys = Object.keys(fontWeights) as FontWeightKey[];
+
+  sizeKeys.forEach(sizeKey => {
+    weightKeys.forEach(weightKey => {
+      const key: Variant = `${sizeKey}-${weightKey}`;
+      variants[key] = {
+        fontSize: size[sizeKey],
+        fontWeight: weight[weightKey],
+      };
+    });
+  });
+
+  return variants;
+};
+
+export type TextVariant =
+  | "f1-regular"
+  | "f1-bold"
+  | "f2-regular"
+  | "f2-bold"
+  | "f3-regular"
+  | "f3-bold"
+  | "f4-regular"
+  | "f4-bold"
+  | "f5-regular"
+  | "f5-bold"
+  | "f6-regular"
+  | "f6-bold"
+  | "f7-regular"
+  | "f7-bold";
+
+export type TextVariants = {
+  [key in TextVariant]: { fontSize: string; fontWeight: string };
+};
+
+export const textVariantsMap = createTextVariants(font.size, font.weight);
