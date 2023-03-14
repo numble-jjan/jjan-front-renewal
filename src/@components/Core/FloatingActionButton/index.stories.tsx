6f8baa94f8ef944874436e0e97525bb113aefa7c
unsnruu <unsnruu@gmail.com>
2023-03-14 20:54:32 +0900

    [#88] 스토리의 args로 element 값을 전달

--- src/@components/Core/FloatingActionButton/index.stories.tsx
+++ src/@components/Core/FloatingActionButton/index.stories.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import FloatingActionButton from "./index";
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 
+import { ReactComponent as WriteIcon } from "@/assets/icons/icon-write.svg";
+import { ReactComponent as UpIcon } from "@/assets/icons/icon-up.svg";
+
 export default {
   title: "Core/FloatingActionButton",
   component: FloatingActionButton,
-  args: {
-    top: "100%",
-    left: "100%",
-  },
 } as ComponentMeta<typeof FloatingActionButton>;
 
 export const Small: ComponentStory<typeof FloatingActionButton> = args => (
   <FloatingActionButton {...args} variant="small" />
 );
+Small.args = {
+  element: UpIcon,
+};
+
 export const Medium: ComponentStory<typeof FloatingActionButton> = args => (
   <FloatingActionButton {...args} variant="medium" />
 );
+Medium.args = {
+  element: WriteIcon,
+};
