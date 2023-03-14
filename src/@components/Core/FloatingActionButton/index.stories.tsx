dede98db61fb9d330e0f954f308d340f5e1756b2
unsnruu <unsnruu@gmail.com>
2023-03-14 21:16:31 +0900

    [#88] feat : Default 스토리를 추가하고 element를 옵션으로 추가

--- src/@components/Core/FloatingActionButton/index.stories.tsx
+++ src/@components/Core/FloatingActionButton/index.stories.tsx
@@ -5,17 +5,30 @@ import type { ComponentMeta, ComponentStory } from "@storybook/react";
 import { ReactComponent as WriteIcon } from "@/assets/icons/icon-write.svg";
 import { ReactComponent as UpIcon } from "@/assets/icons/icon-up.svg";
 
+const IconMap = {
+  writeIcon: WriteIcon,
+  upIcon: UpIcon,
+};
+
 export default {
   title: "Core/FloatingActionButton",
   component: FloatingActionButton,
+  argTypes: {
+    element: {
+      control: "select",
+      options: Object.keys(IconMap),
+      mapping: IconMap,
+    },
+  },
 } as ComponentMeta<typeof FloatingActionButton>;
+
+export const Default: ComponentStory<typeof FloatingActionButton> = args => (
+  <FloatingActionButton {...args} />
+);
 
 export const Small: ComponentStory<typeof FloatingActionButton> = args => (
   <FloatingActionButton {...args} variant="small" />
 );
-Small.args = {
-  element: UpIcon,
-};
 
 export const Medium: ComponentStory<typeof FloatingActionButton> = args => (
   <FloatingActionButton {...args} variant="medium" />
