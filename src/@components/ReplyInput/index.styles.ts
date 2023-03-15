a50bb4b02f0cb2b89fe7bbb0b8cb79f6ab248411
unsnruu <unsnruu@gmail.com>
2023-03-15 20:10:11 +0900

    [#86] feat : Textarea를 사용하여 ReplyInput 구현

--- src/@components/ReplyInput/index.styles.ts
+++ src/@components/ReplyInput/index.styles.ts
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+import { FlexBox } from "../Core/Flexbox";
+import CoreTextArea from "../Core/Textarea";
+import Text from "../Core/Text";
+
+import { color } from "@/styles/theme";
+
+export const Container = styled(FlexBox)`
+  padding: 15px;
+`;
+
+export const AvartarWrapper = styled.div`
+  margin-right: 8px;
+`;
+export const TextContainer = styled(FlexBox)`
+  box-sizing: border-box;
+  border: 1px solid ${color.light_gray1};
+`;
+export const Textarea = styled(CoreTextArea)`
+  flex: 1;
+`;
+export const Button = styled(Text)`
+  cursor: pointer;
+`;
