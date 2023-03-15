9d1e5264c76aff1f953c6467b9c7b77a8187214a
unsnruu <unsnruu@gmail.com>
2023-03-15 21:30:31 +0900

    [#94] feat : FlexItem 세부 구현

--- src/@components/Core/FlexItem/index.styles.ts
+++ src/@components/Core/FlexItem/index.styles.ts
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+import { Props } from "./index";
+
+export const Container = styled.div<Props>(props => ({
+  flex: props.flex,
+  flexGrow: props.flexGrow,
+  flexShrink: props.flexShrink,
+  flexBasis: props.flexShrink,
+}));
--- src/@components/Core/FlexItem/index.tsx
+++ src/@components/Core/FlexItem/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BaseProps } from "../Base/index";
+import { Property } from "csstype";
 
-const FlexItem = () => {
-  return <div></div>;
+import * as Styled from "./index.styles";
+
+export interface Props extends BaseProps {
+  flex?: Property.Flex;
+  flexShrink?: Property.FlexShrink;
+  flexGrow?: Property.FlexGrow;
+  flexBasis?: Property.FlexBasis;
+}
+
+const FlexItem = (props: Props) => {
+  return <Styled.Container {...props} />;
 };
 
 export default FlexItem;
