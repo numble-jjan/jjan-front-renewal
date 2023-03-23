9f6e2805602b83be5c54aaa6c1b0fd43f49fd85d
unsnruu <unsnruu@gmail.com>
2023-03-23 16:11:06 +0900

    [#100] feat : RouterProvider을 연결하고 router 객체 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
 import GlobalStyle from "./styles/GlobalStyle";
 import { RecoilRoot } from "recoil";
 
 import ModalRoot from "./@components/Molecule/Modal";
 
+const router = createBrowserRouter(createRoutesFromElements(<Route></Route>));
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
     <RecoilRoot>
-      {/* <Router /> */}
+      <RouterProvider router={router} />
       <ModalRoot />
     </RecoilRoot>
   </React.StrictMode>,
