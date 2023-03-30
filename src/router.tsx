758e38cc718005b2745b5b3e43d1770e1869476b
unsnruu <unsnruu@gmail.com>
2023-03-30 21:39:08 +0900

    [#116] feat : 정책상 정해진 기본 라우트를 설정

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,59 @@ import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Route,
 } from "react-router-dom";
 
-export const router = createBrowserRouter(createRoutesFromElements(<></>));
+import Landing from "./pages/Landing";
+import SignupCompletion from "./pages/SignupCompletion";
+import Splash from "./pages/Splash";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="splash" element={<Splash />} />
+      <Route path="landing" element={<Landing />} />
+      <Route path="auth">
+        <Route path="signin" />
+        <Route path="signup" />
+        <Route path="signup-complete" element={<SignupCompletion />} />
+        <Route path="signout-complete" />
+      </Route>
+      <Route path="profile">
+        <Route path=":username" />
+      </Route>
+      <Route path="setting">
+        <Route path="profile" />
+        <Route path="location">
+          <Route path="search" />
+        </Route>
+        <Route path="keyword">
+          <Route path="alcohol" />
+          <Route path="mood" />
+        </Route>
+        <Route path="post">
+          <Route path="ventout" />
+          <Route path="soolmate" />
+          <Route path=":postId" />
+          <Route path="write" />
+          <Route path="edit">
+            <Route path=":postId" />
+          </Route>
+        </Route>
+        <Route path="activity">
+          <Route path="post">
+            <Route path=":username" />
+          </Route>
+          <Route path="comment">
+            <Route path=":username" />
+          </Route>
+        </Route>
+        <Route path="comment">
+          <Route path="edit">
+            <Route path=":commentId" />
+          </Route>
+        </Route>
+      </Route>
+    </>,
+  ),
+);
