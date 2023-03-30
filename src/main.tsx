d6ad4ed0346ef885ed78db5317ea35cfd0e473de
unsnruu <unsnruu@gmail.com>
2023-03-30 21:31:44 +0900

    [#116] feat :  router을 별도의 파일로 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 import GlobalStyle from "./styles/GlobalStyle";
 import { RecoilRoot } from "recoil";
 
 import ModalRoot from "./@components/Molecule/Modal";
-
-const router = createBrowserRouter(createRoutesFromElements(<Route></Route>));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
