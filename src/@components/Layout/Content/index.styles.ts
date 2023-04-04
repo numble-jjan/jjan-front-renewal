4821decef8a9c69368d4d63c5fa04a40acecc165
unsnruu <unsnruu@gmail.com>
2023-04-04 19:16:45 +0900

    [#118] feat : header와 bottomNav 각각 경우에 대응하여 높이값 대응

--- src/@components/Layout/Content/index.styles.ts
+++ src/@components/Layout/Content/index.styles.ts
@@ -5,7 +5,12 @@ export const Container = styled.div<{ hasHeader: boolean; hasBottom: boolean }>`
   * margin-bottom이 원하는 방식으로 작동하지 않아서 height값을 수정합니다.
   * 110px = NavIcon(60px) + Header(48px) + g,q와 같은 폰트가 가려지는 걸 막기위한 추가 여유분(2px)
   */
-  height: ${({ hasBottom }) => (hasBottom ? `calc(100% - 110px)` : `100%`)};
+  height: ${({ hasBottom, hasHeader }) => {
+    if (hasHeader && hasBottom) return `calc(100% - 110px)`;
+    if (!hasHeader && hasBottom) return `calc(100% - 62px)`;
+    if (hasHeader && !hasBottom) return `calc(100% - 48px)`;
+    return `100%`;
+  }};
   padding: 0rem 1rem;
   margin-top: ${({ hasHeader }) => (hasHeader ? `48px` : `0px`)};
   overflow: scroll;
