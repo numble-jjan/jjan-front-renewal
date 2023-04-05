3cc8f89bc6ab33b99421349e3789400affa94c2c
unsnruu <unsnruu@gmail.com>
2023-04-05 20:03:40 +0900

    [#121] feat : 랜딩 페이지

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,33 +1,56 @@
 import React from "react";
 
-import Text from "@/@components/Atoms/Text";
-import Button from "@/@components/Atoms/Button";
-import { ReactComponent as Logo } from "@/assets/icons/icon-logo.svg";
 import * as Styled from "./index.styles";
+import Layout from "@/@components/Layout";
+import FlexBox from "@/@components/Core/Flexbox";
+import Text from "@/@components/Core/Text";
+import Button from "@/@components/Core/Button";
 
-const Landing = ({ ...props }) => {
+const LOGO_SRC = "/img-jjan-logo.png";
+
+const Content = () => {
   return (
-    <Styled.Container {...props}>
-      <Logo />
-      <Styled.Introduction>
-        <Styled.Alignment>
-          <Text variant="f1-bold" color="purple">
+    <FlexBox justifyContent="center" alignItems="center" flexDirection="column">
+      <Styled.MarginBottom margin={44}>
+        <img src={LOGO_SRC}></img>
+      </Styled.MarginBottom>
+      <Styled.MarginBottom margin={50}>
+        <Styled.MarginBottom margin={14}>
+          <Text variant="f1-bold" inline={true} color="purple">
             JJAN
           </Text>
-          <Text variant="f1-regular">에 오신 것을 환영합니다.</Text>
-        </Styled.Alignment>
-        <Styled.Alignment direction="column" style={{ lineHeight: "20px" }}>
-          <Text variant="f3-regular">같이 한잔 할 친구가 필요하신가요?</Text>
-          <Text variant="f3-regular">내 주변에서 찾아보세요!</Text>
-        </Styled.Alignment>
-        <Styled.Alignment>
-          <Text variant="f3-bold" color="purple">
-            오늘 하루도 짠~!
+          <Text variant="f1-regular" inline={true}>
+            에 오신 것을 환영합니다!
           </Text>
-        </Styled.Alignment>
-      </Styled.Introduction>
-      <Button shape="purple" height="l" text="JJAN 회원가입" />
-    </Styled.Container>
+        </Styled.MarginBottom>
+        <Styled.MarginBottom margin={30}>
+          <FlexBox flexDirection="column" alignItems="center">
+            <Text variant="f3-regular">같이 한잔 할 친구가 필요하신가요?</Text>
+            <Text variant="f3-regular">내 주변에서 찾아보세요!</Text>
+          </FlexBox>
+        </Styled.MarginBottom>
+        <div>
+          <FlexBox justifyContent="center">
+            <Text variant="f3-bold" color="purple">
+              오늘 하루도 짠- !
+            </Text>
+          </FlexBox>
+        </div>
+      </Styled.MarginBottom>
+      <Button variant="purple" height="l">
+        <Text variant="f3-bold">JJAN 회원가입</Text>
+      </Button>
+    </FlexBox>
+  );
+};
+
+const Landing = () => {
+  return (
+    <Layout>
+      <Layout.Content>
+        <Content />
+      </Layout.Content>
+    </Layout>
   );
 };
 
