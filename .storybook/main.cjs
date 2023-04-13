f58a4abd3e51ed68c46438f6bfe47d2e002a298e
unsnruu <unsnruu@gmail.com>
2023-04-13 21:16:50 +0900

    [#172] chore : 스토리북 업데이트

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,30 +1,25 @@
 const path = require("path");
 const tsconfigPaths = require("vite-tsconfig-paths").default;
 const svgrPlugin = require("vite-plugin-svgr");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
   },
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
-  async viteFinal(config, { configType }) {
-    config.plugins = [
-      ...config.plugins,
-      svgrPlugin({}),
-      tsconfigPaths({
-        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
-      }),
-    ];
-
+  async viteFinal(config, {
+    configType
+  }) {
+    config.plugins = [...config.plugins, svgrPlugin({}), tsconfigPaths({
+      projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")]
+    })];
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
